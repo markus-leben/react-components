990de9d9f3cb2999e00f7ed7592920725f2da769
Markus Leben <markus_leben@mac.com>
2023-02-21 10:57:42 -0800

    done with exercise

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// TODO
+const { useState } = React;
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceries.map(itemName => <GroceryListItem name={itemName}/>)}
+  </ul>
+);
+
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] =  useState(false);
+  const style = {
+    textDecoration: isDone ? 'line-through' : 'none'
+  };
+  return(
+    <li style = {style} onClick={() => setIsDone(!isDone)}>{props.name}</li>
+  )
+}
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceries={['test', '2', 'wwererers']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
